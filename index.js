fa1376d02ab0857005cb055b08c46a8ddb23acbb
sudharani-1 <sudharani.ps01@gmail.com>
2025-07-02 12:44:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use(cors({
     origin: ["http://localhost:5173"],
-    //methods: ["GET", "POST"],
+    methods: ["GET", "POST"],
     credentials: true
 }))
 app.use(cookieParser())
